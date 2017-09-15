9e41c628088ba5b4586e6e66bfe38783e8b1d0f9
Tyler Krys <tylerkrys@gmail.com>
2017-09-15 16:09:41 -0700

    Adds blog/ routes for accessing blog post content

---
+++ routes/blog.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../db').handle;
+const moment = require('moment');
+
+router.get('/', function(req, res) {
+  knex()('posts')
+    .select('*')
+    .then((results) => {
+      if (results.length === 0) {
+        res.status(200).send('No blogs yet');
+        return;
+      }
+      let templateVars = {
+        posts: results
+      }
+      console.log(results);
+      res.render("blog-index", templateVars);
+    });
+});
+
+router.get('/:slug', function (req, res) {
+  knex()('posts')
+    .select('*')
+    .where({
+      'posts.slug': req.params.slug
+    })
+    .then((results) => {
+      if (results.length === 0) {
+        res.status(404).send('No blog post by that name');
+        return;
+      }
+      let templateVars = {
+        post: results[0],
+        timeCreated: moment(results[0].time_created_at).format('MMMM D, YYYY')
+      }
+      console.log(results);
+      res.render("blog", templateVars);
+    });
+});
+
+module.exports = router;
