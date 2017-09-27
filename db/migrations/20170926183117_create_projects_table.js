1dea913256c60843ff94048383eb2d99644317ea
Tyler Krys <tylerkrys@gmail.com>
2017-09-26 23:21:20 -0700

    Adds migration to create projects table in database

---
+++ db/migrations/20170926183117_create_projects_table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(table) {
+    table.increments();
+    table.integer('display_order');
+    table.dateTime('time_created_at').notNull().defaultTo(knex.fn.now());;
+    table.dateTime('time_updated_at').nullable();
+    table.string('name');
+    table.string('slug');
+    table.string('screenshot_url');
+    table.json('tech_stack');
+    table.string('github_url');
+    table.string('github_caption');
+    table.string('secondary_url');
+    table.string('secondary_caption');
+    table.string('deployment_url');
+    table.string('deployment_caption');
+    table.text('body');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('projects');
+};
