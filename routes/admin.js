375eb384ba2ad4bace36a47940e7638edd3bb191
Tyler Krys <tylerkrys@gmail.com>
2017-10-07 22:18:26 -0700

    Adds routes for admin panel

---
+++ routes/admin.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../db').handle;
+const bcrypt = require('bcrypt');
+
+// GET route to /admin, admin panel landing page
+router.get('/', function(req,res) {
+  res.render('admin-panel');
+});
+
+// GET route to /admin/login, login landing page
+router.get('/login', function(req, res) {
+  res.render('admin-login');
+});
+
+// POST route to /admin/login, authentication attempt
+router.post('/login', (req, res) => {
+  knex()
+    .select('*')
+    .from('users')
+    .where({email: req.body.user})
+    .then((results) => {
+      if (results.length === 0) {
+        res.status(401).send('Login failed');
+        return;
+      }
+      if (!bcrypt.compareSync(req.body.password, results[0].password)) {
+        res.status(401).send('Login failed');
+        return;
+      }
+      req.session.userId = results[0].id;
+      res.redirect('/admin');
+    });
+});
+
+// POST route to /admin/logout, end session
+router.post('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/admin/login');
+});
+
+module.exports = router;
