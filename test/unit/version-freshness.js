4a108ba2364c82a4acefd93c1613ca5fe60610cf
Tyler Krys <tylerkrys@gmail.com>
2020-03-02 15:02:44 -0800

    Bumps .travis.yml Node version, adds test for Node version freshness

--- test/unit/version-freshness.js
+++ test/unit/version-freshness.js
@@ -5,6 +5,10 @@ const fs = require('fs')
 const packageJson = require('../../package.json')
 const packageLock = require('../../package-lock.json')
 const changeLog = fs.readFileSync('./CHANGELOG.md', 'utf-8')
+const nvmrc = fs.readFileSync('./.nvmrc', 'utf-8')
+const travisYaml = fs.readFileSync('./.travis.yml', 'utf-8')
+
+console.log(travisYaml)
 
 // Test that versions in package.json, package-lock.json, and CHANGELOG.md match
 describe('Version freshness', () => {
@@ -14,6 +18,14 @@ describe('Version freshness', () => {
   // This assumes CHANGELOG will always have the most recent version at the top
   // of the file as a ## heading, which follows Semantic Versioning.
   const verC = changeLog.substring(changeLog.indexOf('## [') + 4, changeLog.indexOf('] - 20'))
+
+  const verN = nvmrc.trim()
+
+  // Rather that add a new package to parse the Travis YAML config,
+  // just look for the version number as a substring.
+  const verT = travisYaml.substring(travisYaml.indexOf('- "') + 3, travisYaml.length - 2)
+  console.log('verT: ', verT)
+  console.log('verN: ', verN)
 
   it(`package.json v${verP} should match package-lock.json v${verL}`, () => {
     if (verP === verL) {
@@ -33,6 +45,14 @@ describe('Version freshness', () => {
 
   it(`package-lock.json v${verL} should match CHANGELOG.md v${verC}`, () => {
     if (verL === verC) {
+      assert(true)
+    } else {
+      assert(false)
+    }
+  })
+
+  it(`.nvmrc Node v${verN} should match .travis.yml Node v${verT}`, () => {
+    if (verN === verT) {
       assert(true)
     } else {
       assert(false)
